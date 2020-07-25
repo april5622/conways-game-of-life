52e00d5614377bf77b26ddbeb107b872fb3841b6
April Ma <54913304+april5622@users.noreply.github.com>
2020-07-24 17:30:31 -0700

    newsave

--- conways-game-of-life/src/buttons.js
+++ conways-game-of-life/src/buttons.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+// buttons fv
+import React from 'react';
 import { ButtonToolbar } from 'react-bootstrap';
 
 
-export default class buttons extends Component {
-    handleSelect = e => {
-        this.props.gridSize(e)
-    }
-    render() {
-        return (
-            <div className="center">
-                <ButtonToolbar>
-                    <button className="btn btn-default" onClick={this.props.startButton}>Start</button>
-                    <button className="btn btn-default" onClick={this.props.stopButton}>Stop</button>
-                    <button className="btn btn-default" onClick={this.props.clearButton}>Clear</button>
-                    <button className="btn btn-default" onClick={this.props.random}>Random</button>
-                </ButtonToolbar>
-            </div>
-        )
-    }
+function Buttons(props){
+    return (
+        <div className="center">
+            <ButtonToolbar>
+                <button className="btn btn-default" onClick={props.startButton}>Start</button>
+                <button className="btn btn-default" onClick={props.stopButton}>Stop</button>
+                <button className="btn btn-default" onClick={props.clearButton}>Clear</button>                    <button className="btn btn-default" onClick={props.random}>Random</button>
+           </ButtonToolbar>
+        </div>
+    )
 }
+
+
+export default Buttons;
--- conways-game-of-life/src/home.js
+++ conways-game-of-life/src/home.js
@@ -1,48 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Grid from './grid';
 import Buttons from './buttons'
-//import Box from './box';
 import './index.css';
 
-
-export default class home extends Component {
-    constructor(){
-        super();
-        this.speed = 100; // how fast it should be running
-        this.rows = 30; // how many rows are in the grid
-        this.columns = 50 // columns in the grid
-
-        this.state = {
-            generation: 0,
-            gridFull: Array(this.rows).fill().map(() => 
-                Array(this.columns).fill(false) // false because every grid cell is off
-            )
-        }
-    } // constructor
+function Home(){
+    const [speed, setSpeed] = useState(100);
+    const [rows, setRows] = useState(30);
+    const [columns, setColumns] = useState(50)
+    const [generation, setGeneration] = useState(0);
+    const [gridFull, setGridFull] = useState(
+        Array(rows).fill().map(() => 
+        Array(columns).fill(false) // false because every grid cell is off
+    ));
 
 
-    render() {
-        return (
-            <div>
-                <h1>The Game of Life</h1>
-                <Buttons
-                    startButton={this.startButton}
-                    stopButton={this.stopButton}
-                    clearButton={this.clearButton}
-                    random={this.random}
-                />
-                <Grid 
-                />
-                <h2>Generations: {this.state.generation}</h2>
+    return(
+        <div>
+            <h1>The Game of Life</h1>
+            <Buttons
+                startButton={startButton}
+                stopButton={stopButton}
+                clearButton={clearButton}
+                random={random}
+            />
+            <Grid 
+               
+            />
+            <h2>Generations: {generation}</h2>
 
-                <h3>About</h3>
-                <h3>Rules:</h3>
-            </div>
-        )
-    }
+            <h3>About</h3>
+            <h3>Rules:</h3>
+        </div>
+    )
+
 }
 
-function arrayClone(arr) {
-	return JSON.parse(JSON.stringify(arr));
-}
-
+export default Home;

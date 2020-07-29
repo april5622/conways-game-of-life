a99e4a4a24129268cf4f68246e00b42b2a443191
April Ma <54913304+april5622@users.noreply.github.com>
2020-07-29 15:51:29 -0700

    dropbox button

--- conways-game-of-life/src/buttons.js
+++ conways-game-of-life/src/buttons.js
@@ -1,9 +1,12 @@
-// buttons fv
 import React from 'react';
-import { ButtonToolbar } from 'react-bootstrap';
+import { ButtonToolbar, DropdownButton, Dropdown } from 'react-bootstrap';
 
 
 function Buttons(props){
+    const handleSelect = (evt) => {
+        props.gridSize(evt);
+    }
+
     return (
         <div className="center">
             <ButtonToolbar>
@@ -11,6 +14,11 @@ function Buttons(props){
                 <button className="btn btn-default" onClick={props.stopButton}>Stop</button>
                 <button className="btn btn-default" onClick={props.clearButton}>Clear</button> 
                 <button className="btn btn-default" onClick={props.random}>Random</button>
+                <DropdownButton title="Grid Size" id="size-menu" onSelect={handleSelect}>
+                    <Dropdown.Item eventKey="small">Small</Dropdown.Item>
+                    <Dropdown.Item eventKey="medium">Medium</Dropdown.Item>
+                    <Dropdown.Item eventKey="large">Large</Dropdown.Item>
+                </DropdownButton>
            </ButtonToolbar>
         </div>
     )

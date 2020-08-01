4af767a0e86e164fea9b73db6d914e24082d43af
April Ma <54913304+april5622@users.noreply.github.com>
2020-07-31 19:12:12 -0700

    finally

--- conways-game-of-life/src/buttons.js
+++ conways-game-of-life/src/buttons.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import { ButtonToolbar, DropdownButton, Dropdown } from 'react-bootstrap';
 
-
 function Buttons(props){
+
     const handleSelect = (evt) => {
         props.gridSize(evt);
     }
-
     return (
         <div className="center">
             <ButtonToolbar>
-                <button className="btn btn-default" onClick={props.startButton}>Start</button>
-                <button className="btn btn-default" onClick={props.stopButton}>Stop</button>
-                <button className="btn btn-default" onClick={props.clearButton}>Clear</button>
-                <button className="btn btn-default" onClick={props.slowButton}>Slow</button>
-                <button className="btn btn-default" onClick={props.fastButton}>Fast</button> 
-                <button className="btn btn-default" onClick={props.random}>Random</button>
-                <DropdownButton title="Grid Size" id="size-menu" onSelect={handleSelect}>
-                    <Dropdown.Item eventKey="small">Small</Dropdown.Item>
-                    <Dropdown.Item eventKey="medium">Medium</Dropdown.Item>
-                    <Dropdown.Item eventKey="large">Large</Dropdown.Item>
-                </DropdownButton>
-           </ButtonToolbar>
+                    <button className="btn btn-default" onClick={props.startButton}>Start</button>
+                    <button className="btn btn-default" onClick={props.stopButton}>Stop</button>
+                    <button className="btn btn-default" onClick={props.clearButton}>Clear</button>
+                    <button className="btn btn-default" onClick={props.slowButton}>Slow</button>
+                    <button className="btn btn-default" onClick={props.fastButton}>Fast</button> 
+                    <button className="btn btn-default" onClick={props.random}>Random</button>
+                    <DropdownButton title="Grid Size" id="size-menu" onSelect={handleSelect}>
+                        <Dropdown.Item eventKey="small">Small</Dropdown.Item>
+                        <Dropdown.Item eventKey="medium">Medium</Dropdown.Item>
+                        <Dropdown.Item eventKey="large">Large</Dropdown.Item>
+                    </DropdownButton>
+            </ButtonToolbar>
         </div>
-    )
+    ) 
 }
-
 
 export default Buttons;
--- conways-game-of-life/src/home.js
+++ conways-game-of-life/src/home.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react';
+import React, { Component } from 'react';
 import Grid from './grid';
 import Buttons from './buttons'
 import './index.css';
@@ -14,157 +14,161 @@ const directions = [
     [-1, 0]
 ];
 
+export default class home extends Component {
+    constructor(){
+        super();
+        this.speed = 100; // how fast it should be running
+        this.rows = 50; // how many rows are in the grid
+        this.columns = 70; // columns in the grid
 
-function Home(){
-    const [speed, setSpeed] = useState(100);
-    const [rows, setRows] = useState(50);
-    const [columns, setColumns] = useState(70)
-    const [generation, setGeneration] = useState(0);
-    const [gridFull, setGridFull] = useState(
-        Array(rows).fill().map(() =>  // creating an array as big as rows var and fill that with map
-        Array(columns).fill(false) // where an inner array of columns var. false because every grid cell is off
-    ));
+        this.state = {
+            generation: 0,
+            gridFull: Array(this.rows).fill().map(() => 
+                Array(this.columns).fill(false) // false because every grid cell is off
+            )
+        }
+    } // constructor
 
-    const selectCell = (row, column) => {
-        let gridCopy = arrayClone(gridFull); // not updating the state, rather a copy of it
+    selectCell = (row, column) => {
+        let gridCopy = arrayClone(this.state.gridFull);
         gridCopy[row][column] = !gridCopy[row][column];
-        setGridFull(gridCopy);
+        this.setState({
+            gridFull: gridCopy
+        });
     } 
 
-    const random = () => {
-        const gridCopy = arrayClone(gridFull);
-        for (let i = 0; i < rows; i++){
-            for (let j = 0; j < columns; j++){
+    random = () => {
+        const gridCopy = arrayClone(this.state.gridFull);
+        for (let i = 0; i < this.rows; i++){
+            for (let j = 0; j < this.columns; j++){
                 if (Math.floor(Math.random() * 4) === 1){
                     gridCopy[i][j] = true;
                 }
             }
         }
-        setGridFull(gridCopy);
-    };
-
-    let intervalId = useRef(null);
-
-    function startButton(){
-        clearInterval(intervalId);
-        intervalId = setInterval(rule(), speed);
-    }; 
-
-    function stopButton(){
-        clearInterval(intervalId);
-    };
-
-    function clearButton(){
-        const grid = Array(rows).fill().map(() => Array(columns).fill(false));
-        setGridFull(grid);
-        setGeneration(0);
-    };
-
-    function slowButton(){
-        speed(1000);
-        startButton();
+        this.setState({
+            gridFull: gridCopy
+        });
     }
 
-    function fastButton(){
-        speed(100);
-        startButton();
+    startButton = () => {
+        clearInterval(this.intervalId);
+        this.intervalId = setInterval(this.rule, this.speed);
     }
 
-    const gridSize = (size) => {
-        switch (size){
-            case "small":
-                setColumns(20);
-                setRows(10);
-            break;
-            case "medium":
-                setColumns(50);
-                setRows(30);
-            break;
-            default:
-                setColumns(60);
-                setRows(50);
-            
-        }
-        clearButton();
-    };
+    stopButton = () => {
+        clearInterval(this.intervalId);
+    }
 
-    const rule = () => {
-        let g = gridFull;
-        let g2 = arrayClone(gridFull);
-        for (let i = 0; i < rows; i++) {
-            for (let j = 0; j < columns; j++) {
+    slowButton = () => {
+        this.speed = 1000;
+        this.startButton();
+    }
+
+    fastButton = () => {
+        this.speed = 100;
+        this.startButton();
+    }
+
+    clearButton = () => {
+		var grid = Array(this.rows).fill().map(() => Array(this.cols).fill(false));
+		this.setState({
+			gridFull: grid,
+			generation: 0
+		});
+	}
+
+    rule = () => {
+        let g = this.state.gridFull;
+        let g2 = arrayClone(this.state.gridFull);
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.columns; j++) {
                let neighbor = 0;
                 directions.forEach(([x, y]) => {
                   const newI = i + x;
                   const newJ = j + y;
-                  if(newI >= 0 && newI < rows && newJ >= 0 && newJ < columns){
+                  if(newI >= 0 && newI < this.rows && newJ >= 0 && newJ < this.columns){
                       neighbor += g[newI][newJ]
-                  }
+                  };
               });
                 if (g[i][j] && (neighbor < 2 || neighbor > 3)) g2[i][j] = false;
                 if (!g[i][j] && neighbor === 3) g2[i][j] = true;
             }
         }
-        setGridFull(g2);
-        setGeneration(generation + 1);
+        this.setState({
+            gridFull: g2,
+            generation: this.state.generation + 1
+          });
     };
 
-    useEffect(() => {
-        random();
-        setGeneration(1)
-    },[])
-
-    useEffect(() => {
-        //startButton();
-    })
-
-    return(
-        <div>
-            <h1>The Game of Life</h1>
-            <Buttons 
-                startButton={startButton}
-                stopButton={stopButton}
-                clearButton={clearButton}
-                slowButton={slowButton}
-                fastButton={fastButton}
-                random={random}
-                gridSize ={gridSize}
-            />
-            <Grid 
-                gridFull = {gridFull} 
-                rows = {rows}         
-                columns = {columns}
-                selectCell={selectCell}
-            />
+    gridSize = (size) => {
+        switch (size){
+            case "small":
+                this.columns= 20;
+                this.rows = 10;
+            break;
+            case "medium":
+                this.columns=50;
+                this.rows=30;
+            break;
+            default:
+                this.columns=60;
+                this.rows=50;
             
-            <h2>Generations: {generation}</h2>
+        }
+        this.clearButton();
+    };
 
-            <div className="about">
-                <h3>About</h3>
-                <p>
-                    The Game of Life is a cellular automaton devised by British mathematician John
-                    Horton Coway in 1970. It is a zero-player game, which means it evolution is 
-                    determined by its initial stae, requiring no further input. One interacts with
-                    the Game of Life by creating an initial configuration and observing how it evolves.
-                    It is a Turing complete and can simulate a univeral constructor or any other 
-                    Turing machine.
-                </p>
+    componentDidMount() {
+        this.random();
+        this.startButton();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>The Game of Life</h1>
+                <Buttons
+                    startButton={this.startButton}
+                    stopButton={this.stopButton}
+                    clearButton={this.clearButton}
+                    slowButton={this.slowButton}
+                    fastButton={this.fastButton}
+                    random={this.random}
+                    gridSize={this.gridSize}
+                />
+                <Grid 
+                    gridFull = {this.state.gridFull} // these are passed as props 
+                    rows = {this.rows}              // so we can use in grid component
+                    columns = {this.columns}
+                    selectCell={this.selectCell}
+                />
+                <h2>Generations: {this.state.generation}</h2>
+
+                <div className="about">
+                    <h3>About</h3>
+                    <p>
+                        The Game of Life is a cellular automaton devised by British mathematician John
+                        Horton Coway in 1970. It is a zero-player game, which means it evolution is 
+                        determined by its initial stae, requiring no further input. One interacts with
+                        the Game of Life by creating an initial configuration and observing how it evolves.
+                        It is a Turing complete and can simulate a univeral constructor or any other 
+                        Turing machine.
+                    </p>
+                </div>
+
+                <div className="rules">
+                    <h3>Rules: </h3>
+                        <p>1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.</p>
+                        <p>2. Any live cell with two or three live neighbours lives on to the next generation.</p>
+                        <p>3. Any live cell with more than three live neighbours dies, as if by overpopulation.</p>
+                        <p>4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</p>
+                </div>
             </div>
-
-            <div className="rules">
-                <h3>Rules: </h3>
-                <p>1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.</p>
-                <p>2. Any live cell with two or three live neighbours lives on to the next generation.</p>
-                <p>3. Any live cell with more than three live neighbours dies, as if by overpopulation.</p>
-                <p>4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</p>
-            </div>
-        </div>
-    )
-
+        )
+    }
 }
 
 function arrayClone(arr) {
 	return JSON.parse(JSON.stringify(arr));
 }
-
-export default Home;

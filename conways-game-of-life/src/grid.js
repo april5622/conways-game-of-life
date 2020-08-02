24f911ff4ba2a35041fe24ab1f21cc12d009c6b1
April Ma <54913304+april5622@users.noreply.github.com>
2020-08-02 16:59:19 -0700

    fixed error

--- conways-game-of-life/src/grid.js
+++ conways-game-of-life/src/grid.js
@@ -6,12 +6,12 @@ function Grid(props) {
     const width = props.columns * 14;
         const rowsArr = []; // empty array to add everything that will show up in the grid
 
-        const cellClass = ""; // use while creating the array
+        let cellClass = ""; // use while creating the array
         for (var i = 0; i < props.rows; i++){   // will have nested for loop to go through rows & columns
             for (var j = 0; j < props.columns; j++){
                 const cellId = i + "_" + j; // id to go along with each box element
 
-                const cellClass = props.gridFull[i][j] ? "cell alive" : "cell dead" // true if cell is alive or dead
+                cellClass = props.gridFull[i][j] ? "cell alive" : "cell dead" // true if boxz is alive or dead
                 rowsArr.push( //pushing a cell component 
                     <Cell 
                         cellClass={cellClass}
